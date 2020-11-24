9b83eb9e6690f796ff0c1d96b939a9fd4b6b4f34
Jo Price <joprice@developersinstitute.co.nz>
2020-11-24 16:04:40 +0100

    filter update

--- src/components/medicines/medicines.js
+++ src/components/medicines/medicines.js
@@ -2,35 +2,17 @@ import React from 'react';
 import styles from './medicines.module.css';
 
 const Medicines = ({ medicines, searchTerm }) => {
-  console.log(medicines);
-
   const filteredArray = medicines.filter((medicine) => {
-    // medicine.toLowerCase().includes(searchTerm);
     if (
-      medicine.name.startsWith(searchTerm) ||
-      medicine.company.startsWith(searchTerm)
+      medicine.name.includes(searchTerm) ||
+      medicine.company.includes(searchTerm)
     ) {
-      return medicine;
+      return true;
     }
   });
 
   //   const (ternary) = <condition> ? <ifTrue> : <ifFalse>
-  const arrayToRender = searchTerm
-    ? filteredArray(medicines, searchTerm)
-    : medicines;
-
-  //   function
-
-  arrayToRender.map((m, i) => {
-    return (
-      <div>
-        <medicine key={i} name={m.name} company={m.company} />
-        {''}
-      </div>
-    );
-  });
-
-  // const arrayToRender = ({ options,})
+  const arrayToRender = searchTerm ? filteredArray : medicines;
 
   const myMedStyle = {
     textAlign: 'center',
@@ -60,10 +42,11 @@ const Medicines = ({ medicines, searchTerm }) => {
         <div>looks like you have {medicines.length} medicine.</div>
       )}
       looks like you have {medicines.length} drugs!
-      {medicines.map((m, i) => (
+      {arrayToRender.map((m, i) => (
         <div style={myMedStyle} key={i}>
           {' '}
-          name: {m.name}, <br /> company: {m.company}{' '}
+          <div>name: {m.name}</div>
+          <div> company: {m.company}</div>
         </div>
       ))}
     </div>
