9f3d60f32e3ee57a65704f2ce7e8b4b3df61f577
Jo Price <joprice@developersinstitute.co.nz>
2020-11-25 06:12:19 +0100

    fix displayed messages

--- src/components/medicines/medicines.js
+++ src/components/medicines/medicines.js
@@ -1,25 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import styles from './medicines.module.css';
-import Medicine from './medicine/medicine.js';
 
 const Medicines = ({ medicines, searchTerm }) => {
-  //   const [filteredArray, setFilteredArray] = useState([]);
+  const [filteredArray, setFilteredArray] = useState([]);
+  const filterIt = () => {
+    setFilteredArray(
+      medicines.filter((medicine) => {
+        if (
+          medicine.name.toLowerCase().includes(searchTerm) ||
+          medicine.company.toLowerCase().includes(searchTerm)
+        )
+          return true;
+      })
+    );
+  };
 
-  //   const filterIt = () => {
-  //     setFilteredArray(
-  //       medicines.filter((medicine) => {
-  //         if (
-  //           medicine.name.toLowerCase().includes(searchTerm) ||
-  //           medicine.company.toLowerCase().includes(searchTerm)
-  //         )
-  //           return true;
-  //       })
-  //     );
-  //   };
-
-  //   useEffect(() => {
-  //     filterIt();
-  //   }, [searchTerm]);
+  useEffect(() => {
+    filterIt();
+  }, [searchTerm]);
 
   const myMedStyle = {
     textAlign: 'center',
@@ -29,33 +27,28 @@ const Medicines = ({ medicines, searchTerm }) => {
     backgroundColor: 'lightgrey',
   };
 
-  //   if (!filteredArray) {
-  //     return <div>no medicines</div>;
-  //   }
+  if (!filteredArray) {
+    return <div>no medicines</div>;
+  }
+
+  const alert =
+    filteredArray.length === 0
+      ? `looks like you don't have any medicines!`
+      : `looks like you have ${filteredArray.length} medicine${
+          filteredArray.length !== 1 ? 's!' : '!'
+        }`;
 
   return (
     <div className={styles.root}>
-      {/* looks like you have {filteredArray.length} medicines! */}
-
-      {/* {filteredArray.length === 1 && (
-        <div>Looks like you have {filteredArray.length} medicine!</div>
-      )}
-      {filteredArray.length > 1 && (
-        <div>Looks like you have {filteredArray.length} medicines!</div>
-      )}
-      {filteredArray.length < 1 && <div>Looks like you have are healthy</div>} */}
-
-      {medicines.map((m) => (
-        <Medicine name={m.name} company={m.company} />
+      {alert}
+      {filteredArray.map((m, i) => (
+        <div style={myMedStyle} key={i}>
+          {' '}
+          <div>name: {m.name} </div>
+          <div>company: {m.company}</div>
+        </div>
       ))}
     </div>
-
-    //     <div style={myMedStyle} key={i}>
-    //       {' '}
-    //       name: {m.name}, <br /> company: {m.company}{' '}
-    //     </div>
-    //   ))}
-    // </div>
   );
 };
 
