e23f1501c399f28ebf2fe65f1b9525512a4fd865
Jo Price <joprice@developersinstitute.co.nz>
2020-11-25 06:46:32 +0100

    version 1 submission, version 2 in filterBranch

--- src/components/medicines/medicines.js
+++ src/components/medicines/medicines.js
@@ -4,8 +4,8 @@ import styles from './medicines.module.css';
 const Medicines = ({ medicines, searchTerm }) => {
   const filteredArray = medicines.filter((medicine) => {
     if (
-      medicine.name.includes(searchTerm) ||
-      medicine.company.includes(searchTerm)
+      medicine.name.toLowerCase().includes(searchTerm) ||
+      medicine.company.toLowerCase().includes(searchTerm)
     ) {
       return true;
     }
@@ -26,23 +26,17 @@ const Medicines = ({ medicines, searchTerm }) => {
     return <div>no medicines</div>;
   }
 
+  const alert =
+    filteredArray.length === 0
+      ? `looks like you don't have any medicines!`
+      : `looks like you have ${filteredArray.length} medicine${
+          filteredArray.length !== 1 ? 's!' : '!'
+        }`;
+
   return (
     <div className={styles.root}>
-      {medicines.length > 1 && (
-        <div>Looks like you have {medicines.length} medicines!</div>
-      )}
-      {medicines.length == 20 && (
-        <div>
-          You could look at reducing your need for pharmaceutical medication by
-          implementing functional medicine methods.
-          <p>FOOD IS THE REAL MEDICINE</p>{' '}
-        </div>
-      )}
-      {medicines.length == 1 && (
-        <div>looks like you have {medicines.length} medicine.</div>
-      )}
-      looks like you have {medicines.length} drugs!
-      {arrayToRender.map((m, i) => (
+      {alert}
+      {filteredArray.map((m, i) => (
         <div style={myMedStyle} key={i}>
           {' '}
           <div>name: {m.name}</div>
